623392a8e6c434103c349ede81aaebf79ced5d34
eduardomarionucci <dududmdias@gmail.com>
2022-07-19 21:50:07 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <nav-bar logo="Social" url="#" cor="green darken-1" />
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import NavBar from '@/components/layouts/NavBar'
+
+export default {
+  name: 'app',
+  components:{
+    NavBar
+  }
+}
+</script>
+
+<style>
+
+</style>
